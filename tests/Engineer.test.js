22a0cd3be560624d230c300ed7801f11f52c86be
Jesus <perez.jesus1039@gmail.com>
2023-04-18 19:19:16 -0700

    made the tests for engineer

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -1,13 +1,21 @@
 const Engineer = require('../classes/Engineer');
 
+const name = "Han Solo";
+const id = 28;
+const email = "cooldude@email.com";
+const github = "slayer@github";
+const testEngineer= new Engineer(name, id, email, github);
+
 test('Can set GitHUb account via constructor', () => {
-	//add code here
+	expect(typeof(testEngineer)).toBe("object");
 });
 
 test('getRole() should return "Engineer"', () => {
-	//add code here
+	const returnedRole = testEngineer.getRole();
+	expect((returnedRole)).toBe("Engineer");
 });
 
 test('Can get GitHub username via getGithub()', () => {
-	//add code here
+	const returnedGithub = testEngineer.getGithub();
+	expect((returnedGithub)).toBe(github);
 });
