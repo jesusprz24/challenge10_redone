5c136d3b648222727dbb0ef0d5730adadfa436b2
Jesus <perez.jesus1039@gmail.com>
2023-04-18 19:39:23 -0700

    created manager tests

--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -1,14 +1,22 @@
 const Manager = require("../classes/Manager");
 
+const name = "Darth Maul";
+const id = 36;
+const email = "imissmylegs@email.com";
+const officeNumber = "122A"
+const testManager = new Manager(name, id, email, officeNumber);
+
 
 test("Can set office number via constructor argument", () => {
-	//add code here
+	expect(typeof(testManager)).toBe("object");
 });
 
 test("getRole() should return \"Manager\"", () => {
-	//add code here
+	const returnedRole = testManager.getRole();
+	expect((returnedRole)).toBe("Manager");
 });
 
 test("Can get office number via getOffice()", () => {
-	//add code here
+	const returnedofficeNumber = testManager.getOfficeNumber();
+	expect((returnedofficeNumber)).toBe(officeNumber);
 });
