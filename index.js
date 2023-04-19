48b57b4cd8618884907628448e94383dd68860eb
Jesus <perez.jesus1039@gmail.com>
2023-04-18 17:25:28 -0700

    removed comments

--- index.js
+++ index.js
@@ -12,8 +12,6 @@ const getDataReadyForOutput = require('./createTeamHTML.js');
 
 const teamMembers = [];
 
-// First message of the app
-//MUST CHANGE WELCOME MESSAGE
 console.log(
   '\nWelcome to the team generator!\nUse `npm run reset` to reset the dist/ folder\n'
 );
@@ -26,38 +24,26 @@ function startTheApp() {
 			.prompt([
 				{
 					type: 'input',
-					//CHANGE THIS NAME but remember to change it everywhere in the code
-					//this name exactly matches the name we use in .then(answers)
 					name: 'Darth Vader',
 					message: "What is the team manager's name?",
 				},
 				//Manager question 2 here
 				//Manager question 3 here
 				//Manager question 4 here
-
-				//end of manager questions
-				//inquirer uses Promises so we can chain inquirer methods (questions and answers) together (Promises allow developers to use .then())
 			])
 			.then((answers) => {
-				//we are creating a new manager so all of the Manager Class items must be in the answers already
-				//Yes we did ask all of these questions to the user above
 				const manager = new Manager(
-					//here are the answers to the manager questions
 					answers.managerName,
 					answers.managerId,
 					answers.managerEmail,
 					answers.managerOfficeNumber,
 				);
-				// the New Manager Class (A javascript object) is pushed to an empty teamMembers array
 				teamMembers.push(manager);
-				//after the manager is created we move on to a question/decision tree (see next function)
 				askMoreTeamQuestions();
 			});
-		//end createManager() asks inquirer questions and saves the answers to a teamMembers Array
 	}
 
   function askMoreTeamQuestions() {
-    //asks the user to choose which team member to create Engineer, Intern, or None
 		inquirer
 			.prompt([
 				{
@@ -68,7 +54,6 @@ function startTheApp() {
 				},
 			])
 			.then((userChoice) => {
-				// now we move to a switch statement that calls a function based upon the user choice
 				switch (userChoice.memberChoice) {
 					case 'Engineer':
 						createEngineer();
@@ -78,22 +63,16 @@ function startTheApp() {
 						break;
 					default:
             buildTeam();
-            //not an Engineer or an Intern
-            //The user has completed creating a team so start building the team
-					//these three functions are below
 				}
 			});
 	}
 
 	function createEngineer() {
-		//start inquirer questions for the second time with new questions and new answers
 		inquirer
 			.prompt([
 				{
 					type: 'input',
-					//CHANGE THIS NAME but remember to change it everywhere in the code
-					//this name exactly matches the name we use in .then(answers)
-					name: 'engineerName',
+					name: 'Han Solo',
 					message: "What is your engineer's name?",
 				},
 				//Engineer question 2 here
@@ -103,27 +82,21 @@ function startTheApp() {
 			])
 			.then((answers) => {
 				const engineer = new Engineer(
-					//here are the answers to the engineer questions
 					answers.engineerName,
 					answers.engineerId,
 					answers.engineerEmail,
 					answers.engineerGithub,
 				);
-				// the New Engineer Class (A javascript object) is pushed to the teamMembers array
 				teamMembers.push(engineer);
-				//after the engineer is created and pushed we move back to on to the Engineer/Intern/None question/decision tree
 				askMoreTeamQuestions(); //back to line 76
 			});
 	}
 
   function createIntern() {
-		//start inquirer questions for the third time with new questions and new answers
 		inquirer
 			.prompt([
 				{
 					type: 'input',
-					//CHANGE THIS NAME but remember to change it everywhere in the code
-					//this name exactly matches the name we use in .then(answers)
 					name: 'internName',
 					message: "What is your intern's name?",
 				},
@@ -134,32 +107,18 @@ function startTheApp() {
 				//end Intern questions
 			])
 			.then((answers) => {
-				//here are the answers to the intern questions
 				const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool);
-				// the New Intern Class (A javascript object) is pushed to the teamMembers array
 				teamMembers.push(intern);
-				//after the intern is created and pushed we move back to on to the Engineer/Intern/None question/decision tree
 				askMoreTeamQuestions(); //back to line 76
 			});
 	}
 
 	function buildTeam() {
-    //there is a lot going on here in ths one line of code
-    // there are many different code examplesonline that write a file to the file system. Most look similar.
-    // first fs.writeFileSync will create or rewrite a file if it does or does not exist
-    //then the first argument is the path on your file system to the "output directory"
-    //Not sure if the app will work correctly without that directory Keep that directory
-    // the outputPath variable also has the file name that will be used  for the output
-    //then the getDataReadyForOutput function is called and passes in the teamMembers Array
-    // the last option tells the writeFileSync to write the file in UTF-8 format.
+    
 		fs.writeFileSync(outputPath, getDataReadyForOutput(teamMembers), 'utf-8');
 	}
-	//after startTheApp() is called this is the first function call to start the process of asking the questions.
-	//createManager() is run first because only one manager is needed and we get that process done because the app will get more complicated with Engineers and Interns
+	
 	createManager();
 }
-// this function call makes the app run
-// without this call the app will not do anything
-//this app uses a function pattern to run the code it needs it
-// functions are the way developers control which code is run when we want it to run
+
 startTheApp();
