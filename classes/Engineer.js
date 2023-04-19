93c80dc8d405695c459f53580104eac66d6b6a50
Jesus <perez.jesus1039@gmail.com>
2023-04-18 17:16:02 -0700

    removed comments

--- classes/Engineer.js
+++ classes/Engineer.js
@@ -1,10 +1,6 @@
 const Employee = require("./Employee");
 //Must require Employee to connet it and use (use = extend = inherit) all of the parent variables and methods
 class Engineer extends Employee {
-
-	//constructor creates the class
-	//parameters are the all the items the class will use in other parts of the code
-	//super calls the parent Employee and inherits those items (variables ot methods)
 	constructor(name, id, email, github) {
 		super(name, id, email);
 		this.github = github;
